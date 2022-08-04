b6ddb0053eda9ef7f42f935a8a8847099210e42a
beckerinj <becker@assc.ai>
2022-08-04 17:24:09 -0400

    unwrap fails if status not ok

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,4 @@
-use reqwest::Response;
+use reqwest::{Response, StatusCode};
 
 use super::types::{Block, SlackMessageBody};
 
@@ -20,8 +20,8 @@ impl Client {
         &self,
         text: impl Into<String>,
         blocks: impl Into<Option<Vec<Block>>>,
-    ) -> Result<Response, reqwest::Error> {
-        self.http_client
+    ) -> Result<Response, String> {
+        let res = self.http_client
             .post(&self.url)
             .header("Content-Type", "application/json")
             .json(&SlackMessageBody {
@@ -29,21 +29,35 @@ impl Client {
                 blocks: blocks.into(),
             })
             .send()
-            .await
+            .await;
+        match res {
+            Ok(res) => {
+                let status = res.status();
+                if status == StatusCode::OK {
+                    Ok(res)
+                } else {
+                    match res.text().await {
+                        Ok(res) => Err(format!("{status}: {res}")),
+                        Err(e) => Err(format!("{status}: {e:#?}"))
+                    }
+                }
+            }
+            Err(e) => Err(format!("{e:#?}")),
+        }
     }
 
     pub async fn send_message_with_header(
         &self,
         header: impl Into<String>,
         info: impl Into<Option<String>>,
-    ) -> Result<Response, reqwest::Error> {
+    ) -> Result<Response, String> {
         let header: String = header.into();
         let info: Option<String> = info.into();
         let blocks = match &info {
             Some(info) => Some(vec![Block::header(header.clone()), Block::section(info)]),
             None => Some(vec![Block::header(header.clone())]),
         };
-        self.http_client
+        let res = self.http_client
             .post(&self.url)
             .header("Content-Type", "application/json")
             .json(&SlackMessageBody {
@@ -51,10 +65,24 @@ impl Client {
                 blocks,
             })
             .send()
-            .await
+            .await;
+        match res {
+            Ok(res) => {
+                let status = res.status();
+                if status == StatusCode::OK {
+                    Ok(res)
+                } else {
+                    match res.text().await {
+                        Ok(res) => Err(format!("{status}: {res}")),
+                        Err(e) => Err(format!("{status}: {e:#?}"))
+                    }
+                }
+            }
+            Err(e) => Err(format!("{e:#?}")),
+        }
     }
 
-    pub async fn send_mrkdwn_message(&self, text: impl Into<String>) -> Result<Response, reqwest::Error> {
+    pub async fn send_mrkdwn_message(&self, text: impl Into<String>) -> Result<Response, String> {
         let text = text.into();
         self.send_message(&text, vec![Block::section(&text)]).await
     }
