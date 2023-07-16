c0d4e44abae6ae2102d2b8932ea7f19df425f6f4
mbecker20 <becker.maxh@gmail.com>
2023-07-16 15:50:10 +0000

    0.1.0 update deps

--- src/lib.rs
+++ src/lib.rs
@@ -54,24 +54,7 @@ impl Client {
             Some(info) => Some(vec![Block::header(header.clone()), Block::section(info)]),
             None => Some(vec![Block::header(header.clone())]),
         };
-        let res = self
-            .http_client
-            .post(&self.url)
-            .header("Content-Type", "application/json")
-            .json(&SlackMessageBody {
-                text: header,
-                blocks,
-            })
-            .send()
-            .await?;
-
-        let status = res.status();
-        if status == StatusCode::OK {
-            Ok(res)
-        } else {
-            let text = res.text().await.context(format!("status: {status}"))?;
-            Err(anyhow!("status: {status} | text: {text}"))
-        }
+        self.send_message(header, blocks).await
     }
 
     pub async fn send_mrkdwn_message(&self, text: impl Into<String>) -> anyhow::Result<Response> {
